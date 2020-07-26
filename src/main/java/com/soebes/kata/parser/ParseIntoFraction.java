90de86a8797450243486ef4f0935be359efa1485
Karl Heinz Marbaise <kama@soebes.de>
2020-07-26 15:00:18 +0200

    Improved Code.

--- src/main/java/com/soebes/kata/parser/ParseIntoFraction.java
+++ src/main/java/com/soebes/kata/parser/ParseIntoFraction.java
@@ -1,5 +1,24 @@
 package com.soebes.kata.parser;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import com.soebes.kata.fraction.Fraction;
 
 public final class ParseIntoFraction {
@@ -9,11 +28,17 @@ public final class ParseIntoFraction {
   }
 
   /**
+   * A fraction which is given as a string like {@code 4/5}
+   * will be converted into {@link Fraction}.
+   *
    * @param fractionString The Fraction like {@code 4/5}
    * @return {@link Fraction}
    */
-  public static Fraction parseFraction(String fractionString) {
+  public static Fraction parse(String fractionString) {
     String[] split = fractionString.split("/");
+    if (split.length != 2) {
+      throw new IllegalArgumentException(String.format("The format is 'numerator/denominator' given: '%s'", fractionString));
+    }
     int numerator = Integer.parseInt(split[0]);
     int denominator = Integer.parseInt(split[1]);
     return new Fraction(numerator, denominator);
--- src/test/java/com/soebes/kata/parser/ParseIntoFractionTest.java
+++ src/test/java/com/soebes/kata/parser/ParseIntoFractionTest.java
@@ -1,6 +1,26 @@
 package com.soebes.kata.parser;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import com.soebes.kata.fraction.Fraction;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -8,6 +28,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.junit.jupiter.params.provider.Arguments.of;
 
 class ParseIntoFractionTest {
@@ -22,9 +43,15 @@ class ParseIntoFractionTest {
 
   @ParameterizedTest
   @MethodSource("fractionAsString")
-  void several(String fractionString, Fraction expectedFraction) {
-    Fraction fraction = ParseIntoFraction.parseFraction(fractionString);
+  void convert_several_fractions_into_fraction_class(String fractionString, Fraction expectedFraction) {
+    Fraction fraction = ParseIntoFraction.parse(fractionString);
     assertThat(fraction).isEqualTo(expectedFraction);
   }
 
+  @Test
+  void wrong_format_should_fail_with_illegal_argument_exception() {
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> ParseIntoFraction.parse("23"))
+        .withMessage("The format is 'numerator/denominator' given: '23'");
+  }
 }
