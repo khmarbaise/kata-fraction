f23e246fecad63d2e4a141178ceebba97559d91b
Karl Heinz Marbaise <kama@soebes.de>
2020-09-23 22:33:02 +0200

    Improved first working dynamic tests working.

--- src/test/java/com/soebes/kata/fraction/dynamic/DynamicFractionTest.java
+++ src/test/java/com/soebes/kata/fraction/dynamic/DynamicFractionTest.java
@@ -2,12 +2,15 @@ package com.soebes.kata.fraction.dynamic;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicNode;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.platform.commons.support.ReflectionSupport;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -20,38 +23,62 @@ class DynamicFractionTest {
 
     private static final Predicate<Class<?>> isNotInterfaceAndNotMember = s -> !s.isInterface()
             && !s.isMemberClass()
-            && s.getSimpleName().startsWith("Fraction");
+            && s.getSimpleName().equals("Fraction");
     private static final Predicate<String> isNoTest = s -> !s.endsWith("Test");
 
-    @Test
-    void name() {
+    @TestFactory
+    @DisplayName("Classes")
+    Stream<DynamicNode> allClassesTest() {
         List<Class<?>> allClassesInPackage = ReflectionSupport
                 .findAllClassesInPackage("com.soebes.kata.fraction", isNotInterfaceAndNotMember, isNoTest);
-        allClassesInPackage.forEach(getXXXX());
 
+        return allClassesInPackage.stream()
+                .map(theClass ->
+                    dynamicContainer(theClass.getSimpleName(), Stream.of(
+//                            dynamicTest("isNotNullTest-" + theClass.getSimpleName(), isNotNullTest(theClass)),
+//                            dynamicTest("T2-" + theClass.getSimpleName(), () -> assertThat(theClass.isInterface()).isFalse()),
+                            dynamicTest("1/3+2/3 = 3/3 plus()", () -> addition_1_3_plus_2_3(theClass))
+                        )
+                    )
+                );
     }
 
-    private Consumer<Class<?>> getXXXX() {
-        return aClass -> {
-            System.out.println("aClass = " + aClass.getName() + " " + aClass.getNestMembers() + " Host:" + aClass.getNestHost() + " X:" + aClass.isNestmateOf(aClass));
-            Class<?>[] nestMembers = aClass.getNestMembers();
-            for (int i = 0; i < nestMembers.length; i++) {
-                System.out.println("nestMembers[" + i + "]= " + nestMembers[i].getName());
-            }
-        };
+    private Executable isNotNullTest(Class<?> aClass) {
+        return () -> assertThat(aClass.getSimpleName()).isNotNull();
     }
 
-    @TestFactory
-    @DisplayName("First tests")
-    Stream<DynamicNode> dynamicTestsWithContainers() {
-        return Stream.of("A", "B", "C")
-                .map(input -> dynamicContainer("Container " + input, Stream.of(
-                        dynamicTest("not null", () -> assertThat(input).isNotNull()),
-                        dynamicContainer("properties", Stream.of(
-                                dynamicTest("length > 0", () -> assertThat(input).isNotNull()),
-                                dynamicTest("not empty", () -> assertThat(input).isEmpty())
-                        ))
-                )));
+    private Executable addition_1_3_plus_2_3(Class<?> aClass) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Constructor<?>[] theConstructors = aClass.getConstructors();
+        if (theConstructors.length != 1) {
+            throw new IllegalArgumentException("Mehr als ein CTor definiert.");
+        }
+
+        Class<?>[] parameterTypes = theConstructors[0].getParameterTypes();
+
+        Constructor<?> constructors = aClass.getConstructor(parameterTypes);
+
+        Object summand_1 = constructors.newInstance(convertToParameterType(parameterTypes[0], 1L), convertToParameterType(parameterTypes[0], 3L));
+        Object summand_2 = constructors.newInstance(convertToParameterType(parameterTypes[0], 2L), convertToParameterType(parameterTypes[0], 3L));
+
+        Method plusMethod = summand_1.getClass().getMethod("plus", summand_1.getClass());
+
+        Object sum = plusMethod.invoke(summand_1, summand_2);
+
+        Object expectedResult = constructors.newInstance(convertToParameterType(parameterTypes[0], 1L), convertToParameterType(parameterTypes[0], 2L));
+        assertThat(sum).isEqualTo(expectedResult);
+        return () -> {};
+    }
+
+    Object convertToParameterType(Class<?> parameterType, Long value) {
+        if (int.class.equals(parameterType)) {
+            return Integer.valueOf(value.intValue());
+        } else if (long.class.equals(parameterType)) {
+            return Long.valueOf(value);
+        } else if (BigInteger.class.equals(parameterType)) {
+            return BigInteger.valueOf(value);
+        } else {
+            throw new IllegalArgumentException("Unbekannter Typ");
+        }
     }
 
 
