80b250e7332b4b88d7f58f7ffb8d394e6f2a1820
Karl Heinz Marbaise <kama@soebes.de>
2021-03-18 20:42:30 +0100

    First step to try to modularize.

---
+++ src/test/java/java-module/module-info.java
@@ -0,0 +1,10 @@
+open module com.soebes.kata.fraction {
+  requires java.base;
+
+  requires org.junit.jupiter.api;
+  requires org.junit.platform.engine;
+  requires org.assertj.core;
+
+  requires org.apiguardian.api;
+
+}
