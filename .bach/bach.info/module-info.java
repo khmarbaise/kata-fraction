a7863a7fbfe1da0fbc355359ca318366c4135dfd
Karl Heinz Marbaise <kama@soebes.de>
2021-03-18 21:23:12 +0100

    Improved.

--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -1,8 +1,16 @@
 import com.github.sormuras.bach.ProjectInfo;
+import com.github.sormuras.bach.ProjectInfo.Tweak;
+import com.github.sormuras.bach.ProjectInfo.Tools;
+
 @ProjectInfo(
     compileModulesForJavaRelease = 16,
     modules = "src/main/java",
-    testModules = "src/test/{java,java-module}"
+    testModules = "src/test/{java,java-module}",
+    requires = "org.junit.platform.console",
+    tools = @Tools(skip = "javadoc"),
+    testTweaks = {
+        @Tweak(tool = "junit(com.soebes.kata.fraction)", option = "--fail-if-no-tests"),
+    }
 )
 module bach.info {
   requires com.github.sormuras.bach;
