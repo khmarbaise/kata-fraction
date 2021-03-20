3c219c7ecda631e193db2f9abd8e627e85784921
Christian Stein <sormuras@gmail.com>
2021-03-20 06:13:11 +0000

    Manage dependencies via lookups

--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -1,13 +1,16 @@
 import com.github.sormuras.bach.ProjectInfo;
-import com.github.sormuras.bach.ProjectInfo.Tweak;
-import com.github.sormuras.bach.ProjectInfo.Tools;
+import com.github.sormuras.bach.ProjectInfo.*;
 
 @ProjectInfo(
     compileModulesForJavaRelease = 16,
-    modules = "src/main/java",
-    testModules = "src/test/{java,java-module}",
-    requires = {"org.junit.platform.console", "org.junit.jupiter", "org.assertj.core"},
     tools = @Tools(skip = {"jdeps", "javadoc", "jlink"}),
+    requires = {"org.junit.platform.console", "org.junit.jupiter", "org.assertj.core"},
+    lookupExternal = {
+      @External(module = "org.assertj.core", via = "org.assertj:assertj-core:3.19.0"),
+    },
+    lookupExternals = {
+      @Externals(name = Externals.Name.JUNIT, version = "5.7.1"),
+    },
     testTweaks = {
         @Tweak(tool = "junit(com.soebes.kata.fraction)", option = "--fail-if-no-tests"),
     }
