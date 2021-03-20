96894fefb27923a133eb47ed9a4e4b7b94ae2178
Christian Stein <sormuras@gmail.com>
2021-03-20 07:31:12 +0000

    Upgrade to Bach 17-ea+4d8774a

--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -4,16 +4,10 @@ import com.github.sormuras.bach.ProjectInfo.*;
 @ProjectInfo(
     compileModulesForJavaRelease = 16,
     tools = @Tools(skip = {"jdeps", "javadoc", "jlink"}),
-    requires = {"org.junit.platform.console", "org.junit.jupiter", "org.assertj.core"},
-    lookupExternal = {
-      @External(module = "org.assertj.core", via = "org.assertj:assertj-core:3.19.0"),
-    },
-    lookupExternals = {
-      @Externals(name = Externals.Name.JUNIT, version = "5.7.1"),
-    },
-    testTweaks = {
-        @Tweak(tool = "junit(com.soebes.kata.fraction)", option = "--fail-if-no-tests"),
-    }
+    requires = "org.junit.platform.console",
+    lookupExternal = @External(module = "org.assertj.core", via = "org.assertj:assertj-core:3.19.0"),
+    lookupExternals = @Externals(name = Externals.Name.JUNIT, version = "5.7.1"),
+    testTweaks = @Tweak(tool = "junit", option = "--fail-if-no-tests")
 )
 module bach.info {
   requires com.github.sormuras.bach;
