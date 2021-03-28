6103c5a330fb7d603c25d21faa571f534e91bd6a
Karl Heinz Marbaise <kama@soebes.de>
2021-03-28 18:38:05 +0200

    cleaned up ProjectInfo.

--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -12,7 +12,6 @@ import com.github.sormuras.bach.ProjectInfo.Metadata.Checksum;
         @External(module = "org.junit.jupiter", via = "org.junit.jupiter:junit-jupiter:5.7.1"),
         @External(module = "org.junit.platform.console", via = "org.junit.platform:junit-platform-console:1.7.1"),
     },
-//    lookupExternal = @External(module = "org.assertj.core", via = "org.assertj:assertj-core:3.19.0"),
     lookupExternals = @Externals(name = Externals.Name.JUNIT, version = "5.7.1"),
     testTweaks = @Tweak(tool = "junit", option = "--fail-if-no-tests"),
     metadata = {
