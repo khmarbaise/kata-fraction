cbb311f2920281280bd95f1d11f9244d8e7e12a8
Karl Heinz Marbaise <kama@soebes.de>
2021-09-15 17:49:14 +0200

    Fixed.

--- test.com.soebes.katas.fraction/src/test/java/module-info.java
+++ test.com.soebes.katas.fraction/src/test/java/module-info.java
@@ -2,7 +2,8 @@ open module test.com.soebes.katas.fraction {
   requires java.base;
 
   requires org.assertj.core;
-  requires org.junit.jupiter;
+  requires org.junit.jupiter.api;
+  requires org.junit.platform.commons;
 
 	requires com.soebes.katas.fraction;
 }
