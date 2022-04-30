ebed0928cc1f3fe5be19f860d118804f40f27e09
Karl Heinz Marbaise <kama@soebes.de>
2022-04-30 14:52:38 +0200

    Reformatted.

--- src/test/java/com/soebes/kata/fraction/FractionIntegerTest.java
+++ src/test/java/com/soebes/kata/fraction/FractionIntegerTest.java
@@ -25,7 +25,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
